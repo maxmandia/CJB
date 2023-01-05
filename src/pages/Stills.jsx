a7a9acda68d96fc24e3d9faa516733412b245c16
Max Mandia <mmandia511@gmail.com>
2023-01-04 22:44:53 -0500

    feat: added folders

--- src/pages/Stills.jsx
+++ src/pages/Stills.jsx
@@ -3,8 +3,12 @@ import { Context } from "../App";
 import Header from "../components/Header";
 import SideBar from "../components/SideBar";
 import "../styles/stills.css";
-import YellowFolder from "../assets/yellow-folder.png";
 import { Link } from "react-router-dom";
+import Orange from "../assets/orange_01.png";
+import Yellow from "../assets/yellow.png";
+import Blue from "../assets/blue_02.png";
+import Green from "../assets/green_02.png";
+import Pink from "../assets/pink_02.png";
 
 function Stills() {
   const { showNav } = useContext(Context);
@@ -16,31 +20,31 @@ function Stills() {
       <div className="all-folders-container">
         <Link style={{ "text-decoration": "none" }} to={"/stills/portraits"}>
           <div className="folder-container">
-            <img className="folder" src={YellowFolder} alt="folder" />
+            <img className="folder" src={Orange} alt="folder" />
             <p>PORTRAITS</p>
           </div>
         </Link>
         <Link style={{ "text-decoration": "none" }} to={"/stills/lifestyle"}>
           <div className="folder-container">
-            <img className="folder" src={YellowFolder} alt="folder" />
+            <img className="folder" src={Yellow} alt="folder" />
             <p>LIFESTYLE</p>
           </div>
         </Link>
         <Link style={{ "text-decoration": "none" }} to={"/stills/landscapes"}>
           <div className="folder-container">
-            <img className="folder" src={YellowFolder} alt="folder" />
+            <img className="folder" src={Blue} alt="folder" />
             <p>LANDSCAPE</p>
           </div>
         </Link>
         <Link style={{ "text-decoration": "none" }} to={"/stills/Architecture"}>
           <div className="folder-container">
-            <img className="folder" src={YellowFolder} alt="folder" />
+            <img className="folder" src={Green} alt="folder" />
             <p>ARCHITECTURE</p>
           </div>
         </Link>
         <Link style={{ "text-decoration": "none" }} to={"/stills/Jewelry"}>
           <div className="folder-container">
-            <img className="folder" src={YellowFolder} alt="folder" />
+            <img className="folder" src={Pink} alt="folder" />
             <p>JEWELRY</p>
           </div>
         </Link>
