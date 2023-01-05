d92642c3f1f9f12af077e4adccb869944e517a09
Max Mandia <mmandia511@gmail.com>
2023-01-05 16:55:14 -0500

    feat: added other images

--- src/pages/Other.jsx
+++ src/pages/Other.jsx
@@ -1,12 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { Context } from "../App";
+import Header from "../components/Header";
+import SideBar from "../components/SideBar";
 import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
 import { firebaseConfig } from "../firebaseConfig";
 import { initializeApp } from "firebase/app";
+import "../styles/other.css";
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 
 function Other() {
-  const [images, setImages] = useState([null]);
+  const { showNav } = useContext(Context);
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
     const listRef = ref(storage, "MotionGrabs/OtherWork");
@@ -17,6 +22,7 @@ function Other() {
           for (const img of res.items) {
             let item = await getDownloadURL(img);
             console.log(item);
+
             setImages((current) => [...current, item]);
           }
         }
@@ -27,7 +33,20 @@ function Other() {
       });
   }, []);
 
-  return <div>Other</div>;
+  useEffect(() => {
+    console.log(images);
+  }, [images]);
+
+  return (
+    <div className="other-container">
+      <Header isBlack={true} />
+      {showNav && <SideBar />}
+      {images.length > 0 &&
+        images.map((image) => {
+          return <img className="other-image" src={image} alt="other work" />;
+        })}
+    </div>
+  );
 }
 
 export default Other;
