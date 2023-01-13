d24213c837d1186f99f63a39aee158359622b49c
Max Mandia <mmandia511@gmail.com>
2023-01-13 09:06:27 -0800

    feat: added new bio

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,17 +17,26 @@ function About() {
       <div className="about-container">
         <img src={Cullen} alt="me" />
         <p className="bio">
-          I'm passionate about elevating stories visually. Whether it is an
-          individual's life experience, a brand's mission, or simply a group's
-          purpose, my favorite challenge is transforming these expressions into
-          visuals.
+          I've had the pleasure of growing up and living around the United
+          States including Upstate New York, The Bay Area in California,
+          Atlanta, Georgia, and Dallas, Texas. This has sparked my curiosity
+          about the variety of life experiences around the world, and how
+          important it is to share those stories that aren’t easily accessed.
         </p>
         <p>&nbsp;&nbsp;&nbsp;&nbsp;</p>
         <p className="bio">
-          Through a BFA Film Production degree from SMU, starting my personal
-          production company, and working with acclaimed photographer and
-          director Stewart Cohen for 2 years out of Dallas, I am able to bring a
-          plethora of skills and experience to a production.
+          I'm passionate about elevating stories visually. Whether it is an
+          individual's life experience, a brand's mission, or simply a group's
+          purpose, my favorite challenge is transforming these expressions into
+          visuals. Cinematically representing an individual's emotional
+          experience is my favorite challenge as a director and cinematographer.
+        </p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;</p>
+        <p className="bio">
+          Through a BFA Film Production degree from SMU, starting my production
+          company, and working with acclaimed photographer and director Stewart
+          Cohen for 2 years out of Dallas, I can bring a plethora of skills and
+          experience to a production.
         </p>
         <p>&nbsp;&nbsp;&nbsp;&nbsp;</p>
         <p className="bio">Let’s create something to inspire.</p>
